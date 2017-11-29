bfb9b84281a52badc251970a3adf0cac4f4aac69
Prateek Madaan <31117403+prateek951@users.noreply.github.com>
2017-11-29 19:36:31 +0530

    Add files via upload

---
+++ scope.py
@@ -0,0 +1,24 @@
+#LOCAL SCOPE
+
+
+
+my_name = 'ryu' #GLOBAL SCOPE VARIABLE
+
+
+def print_name():
+
+	#HERE WE ARE OVERRIDING THE GLOBAL VARIABLE
+
+	global my_name
+
+	my_name = 'prateek'
+
+	print('The name inside the function is',my_name)
+
+	#prateek
+
+print_name()
+
+print('Outside the function the name is',my_name)
+
+	#prateek
