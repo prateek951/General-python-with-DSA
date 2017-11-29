50a1f6748f0dfb30a8b23b69878d211bf0a7c7cb
Prateek Madaan <31117403+prateek951@users.noreply.github.com>
2017-11-29 19:57:05 +0530

    Add files via upload

---
+++ Car.py
@@ -0,0 +1,65 @@
+class Car:
+
+	#CLASS VARIABLE
+
+	wheelsShape = 'Round'	# EVERY CAR'S WHEELS ARE ROUND IN SHAPE!!
+
+	def __init__(self,args):
+
+		# SETTING THE NAME OF THE CAR INSTANCE
+
+		self.name = name
+
+		# SETTING THE MODEL OF THE CAR INSTANCE
+
+		self.model = model
+
+		#BOOLEAN FOR SETTING WHETHER THE CAR RUNS ON PETROL
+
+		self.runsOnPetrol = runsOnPetrol
+
+		self.speed = speed
+
+	def speedChecker(speed)
+
+		if(speed > 100)
+
+			print('The car is blazingly fast!! ACCEPTED')
+
+		elif (speed > 60 && speed <= 100):
+
+			print('The car catches up moderate speed!!! SOMEWHAT ACCEPTABLE WILL THINK')
+
+		else 
+			print('The car is relatively slow REJECTED!!')
+		
+# CREATE A NEW CAR INSTACE
+
+mercedes = Car('Mercedes','E-CLASS',True,500)
+
+# PRINT INFORMATION ABOUT THE CAR
+
+print(f'The name of the car is {mercedes.name}')
+
+print(f'The model of the car is {mercedes.model}')
+
+print(f'This car runs on petrol : {mercedes.runsOnPetrol}')
+
+print(f'The speed of the car is : {mercedes.speed}')
+
+print(f'TESTING FOR WHETHER THE CAR CAN BE LAUNCHED ON THE ROADS!!')
+
+
+bound = 5
+
+for 0 in 5:
+	print('#################################','\n')
+
+print(speedChecker(mercedes.speed));
+
+
+
+
+
+
+		
