d5334e17f95daf6d48259ccc7cf2c1e6092bd4ec
Prateek Madaan <31117403+prateek951@users.noreply.github.com>
2017-11-30 12:06:54 +0530

    Update area_pi.py

--- area_pi.py
+++ area_pi.py
@@ -1,15 +1,7 @@
-	
-
-#prateek,you are 21
-
 # CALCULATE THE AREA OF THE CIRCLE
-
 radius = input('Enter the radius of the circle')
-
 # HERE WE ARE DOING TYPECASTING
-
 area = 3.142*int(radius)**2;
-
 print('The area of the circle is',area)
 
 
