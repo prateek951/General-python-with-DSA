6b310ed4a6549383458c5b4f99239452c5c2ec82
Prateek Madaan <31117403+prateek951@users.noreply.github.com>
2017-11-30 13:05:46 +0530

    setsortingdemo

--- setssorting.py
+++ setssorting.py
@@ -15,37 +15,29 @@ for num in nums
 	print(num)	# Print the numbers in any order since it is a set each time we will get a different order in which the items get printed
 
 
-# CREATE A DICTIONARY
-
+# Create a dictionary of ninjas(A dictionary is collection of key-values pairs just like object-literals in javascript)
 ninjas = {
-	
 	'ryu' : 'black',
-
 	'yoshi' : 'red',
-
-
 	'prateek' : 'yellow'
-
 }
 
-print(ninjas.values())
+print(ninjas.values())	# Prints all the values in the above key values pairs in the form of a list
 
-set(ninjas.values())   #black red yellow
+set(ninjas.values())   #Makes a set of the values in above key value pairs but each time in different order.
 
-# CALCULATE HOW MANY INSTANCES ARE OF A PARTICULAR BELT COLOR
+# Method to calculate how many instances are there for a particular belt color!
 
 def countInstances(dictionary):
 	
-
-	# FIRST MAKE A LIST OF ALL THE BELTS
-
+	# First make a list of all the dictionary values
 	belts = list(dictionary.values())
 
 	#cycle over all the belts
 
-	for belt in set(belts)	# WE WANT UNIQUENESS 
+	for belt in set(belts)	# Make a set of belts to maintain uniqueness as set does't allow duplicacies!
 
-		belt = belts.count(belt)
+		belt = belts.count(belt)	#Count the instances of each belt!!
 
 		print(f'There are {num} {belt} belts')
 
