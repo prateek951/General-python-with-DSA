dbcb3a0222bef11fd55ad2e3125c9d92afaf1f56
Prateek Madaan <31117403+prateek951@users.noreply.github.com>
2017-11-30 12:55:32 +0530

    Update scope.py

--- scope.py
+++ scope.py
@@ -4,11 +4,11 @@ def print_name():
 
 	global my_name	#Using the same my_name variable that is defined outside this block at the top!
 
-	my_name = 'prateek'	#Here we are overriding the global variable(so now 
+	my_name = 'prateek'	#Here we are overriding the global variable(so now my_name is now prateek both in the local as well as the global scope)
 
 	print('The name inside the function is',my_name)
 
-	#prateek
+	#prints prateek
 
 print_name()
 
