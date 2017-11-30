f2443939a6ab5c8d0c902585a9a8d2096a27edd5
Prateek Madaan <31117403+prateek951@users.noreply.github.com>
2017-11-30 13:08:43 +0530

    Update stringformatting.py

--- stringformatting.py
+++ stringformatting.py
@@ -13,13 +13,12 @@ print('Number 1 is',num1,'Number 2 is',num2)
 
 print('Number 1 is {0} and Number 2 is {1}'.format(num1,num2))
 
-# TO SET PRECISION WE CAN USE
-
+# To set precision we can use : after the index and specifying the number of decimal places!!
 #3 decimal places
 
 print('Number 1 is {0:3} and Number 2 is {1}'.format(num1,num2))
 
-# USING THE FORMAT-METHOD
+# Using the format method
 
 print('Number 1 is {0:3f} and Number 2 is {1}'.format(num1,num2))
 
