9c1ec689cd3cb18ed4df3e12058797f026e9ce57
Prateek Madaan <31117403+prateek951@users.noreply.github.com>
2017-11-30 12:48:32 +0530

    Update ranges.py

--- ranges.py
+++ ranges.py
@@ -8,14 +8,13 @@ for n in range(3,10):
 	
 	#Looping over the range from 0 to 10-1(excluding 10)
 	print(n)
-
 	# 3 4 5 6 7 8 9 is the output
 
 for n in range(0,20,4):
 	
+	#Looping over the range from 0 to 20-1(excluding 20)
 	print(n)
-
-	# 0 4 8 12 16
+	# 0 4 8 12 16 is the output each time we go into the loop we get an increment of 4
 
 friends = ['prateek','nischal','navneet']
 
