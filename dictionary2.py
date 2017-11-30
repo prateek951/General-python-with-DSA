ab4a6230c0676cc2bd90ac0dad828df8960eb2a8
Prateek Madaan <31117403+prateek951@users.noreply.github.com>
2017-11-30 12:15:14 +0530

    Update dictionary2.py

--- dictionary2.py
+++ dictionary2.py
@@ -11,20 +11,11 @@ while True:
 	else:
 		break
 
-# NOW WE ARE HAVING A DICTIONARY OF NINJAS
-
 def ninja_details(dictionary):
 	
-	# cycle through all the ninjas
-
-	# Note a ninjas is a dictionary of key and values
-
 	for key,val in dictionary.items():
 
 		print(f'My name is {key} and I am a {val}-belter')
-
-
-# INVOKE THE ninja_details function
 
 ninja_details(ninjas)
 
