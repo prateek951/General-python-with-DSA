f4a200c53e01d9a9b1659abdba69a3147a4653c2
Prateek Madaan <31117403+prateek951@users.noreply.github.com>
2017-12-21 22:23:43 +0530

    completed

--- dictionaries.py
+++ dictionaries.py
@@ -1,18 +1,40 @@
+# DICTIONARIES
+
+# MAPPING TYPES INTERSECT KEY VALUES
+
+# SAME AS JS OBJECT notation
+
 ninja_belts = {
 	
 	"crystal" : 'red',
 
 	"ryu" : 'black'
 }
-print(ninja_belts['crystal'])	
-print(ninja_belts['ryu'])
-print('yoshi' in ninja_belts) 
-print(ninja_belts.keys()) 
-print(list(ninja_belts.keys())
-print(ninja_belts.values())
+
+print(ninja_belts['crystal'])	#red
+
+print(ninja_belts['ryu']) #black
+
+print('yoshi' in ninja_belts) #yoshi is not in the dictionary
+
+print(ninja_belts.keys()) # returns the keys of the dictionary
+
+print(list(ninja_belts.keys()) #typecasting it as a result
+
+print(ninja_belts.values()) # return red,black
+
 vals = list(ninja_belts.values())
-print(vals.count('red'))
+
+# Find the instances of a particular value
+
+print(vals.count('red')) 	# 1
+
+# ADDING A NEW NINJA TO THE DICTIONARY
+
 ninja_belts['yoshi'] = 'red'
+
+# ALTERNATE WAY OF DEFINING NEW DICTIONARY
+
 person = dict(name ='Prateek',age=21,height='6ft')
 
 
