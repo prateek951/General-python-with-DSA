5042bfab6ac20d8d14091cf3ba5ca611eee8ac8d
Prateek Madaan <31117403+prateek951@users.noreply.github.com>
2017-12-21 22:28:08 +0530

    module completed

---
+++ module.py
@@ -0,0 +1,12 @@
+from classes import Planet
+
+# import the Planet Class
+
+venus = Planet('Venus',30000,8,'Solar System')
+
+print(f'Name : {venus.name}')
+
+print(venus.orbit());
+
+# PACKAGES ARE COLLECTION OF MODULES
+
