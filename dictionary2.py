0e3b69f4b424a799098327d6de94cddd5b999cc2
Prateek Madaan <31117403+prateek951@users.noreply.github.com>
2017-12-21 22:23:13 +0530

    using dictionaries

--- dictionary2.py
+++ dictionary2.py
@@ -1,21 +1,41 @@
+# create an empty dictionary
+
 ninjas = {}
+
 while True:
 	
 	ninja_name = input('Enter the ninja name')
+
 	ninja_belt = input('Enter the ninja belt')
+
 	ninjas[ninja_name] = ninja_belt
+
+	# Type y to stop entering ninjas
+
 	choice = input('Want more ninjas')
+
 	if choice == 'y':
 	
 		continue
+	
 	else:
+		
 		break
+
+# NOW WE ARE HAVING A DICTIONARY OF NINJAS
 
 def ninja_details(dictionary):
 	
+	# cycle through all the ninjas
+
+	# Note a ninjas is a dictionary of key and values
+
 	for key,val in dictionary.items():
 
 		print(f'My name is {key} and I am a {val}-belter')
+
+
+# INVOKE THE ninja_details function
 
 ninja_details(ninjas)
 
